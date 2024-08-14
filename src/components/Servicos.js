63b5afc7fefa84d0c11f5f74daab97749bd683de
Rafael Lélis De Carvalho <124531634+rafinhahdc19@users.noreply.github.com>
2024-08-13 21:09:52 -0300

    Update Servicos.js

--- src/components/Servicos.js
+++ src/components/Servicos.js
@@ -12,7 +12,7 @@ export default function Servicos(){
                     <Image className="mx-3 mb-4 flex items-center justify-center w-auto sm:max-w-xs rounded-2xl" width={473} height={473} src="/Servicosimage.jpg" alt="serviços image"/>
                 </div>
                 <div className="mx-3">
-                    <h3 className="sm:text-3xl text-2xl font-sans font-semibold">Faça tudo com segurança.</h3>
+                    <h3 className="sm:text-3xl text-2xl font-sans font-semibold">Faça tudo com segurança</h3>
                     <p className="sm:text-xl text-base mb-4 md:mb-0 font-sans font-normal">Nossos serviços abrangem treinamento especializado em tiro esportivo, além de eventos e competições emocionantes. Conte com nossa equipe de profissionais para uma experiência segura e enriquecedora. Explore o universo do tiro esportivo conosco e alcance novos patamares em sua habilidade e técnica de tiro.</p>
                 </div>
             </div>
